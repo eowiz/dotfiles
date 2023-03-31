0547d1f9ded92bf2c217c0afa3fadab072d70b34
eowiz <eowiz@proton.me>
2023-04-01 03:16:14 +0900

    fix(zsh): install sdkman with homebrew

--- dot_zsh/config/sdkman.zsh
+++ dot_zsh/config/sdkman.zsh
@@ -1,2 +1,2 @@
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"

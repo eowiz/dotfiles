08e9a85d238b8c7d4643787c4a0da5068751fe6c
eowiz <eowiz@proton.me>
2023-04-19 00:54:53 +0900

    feat(zsh): add path to rust-analyer

--- dot_zsh/config/cargo.zsh
+++ dot_zsh/config/cargo.zsh
@@ -1,2 +1,7 @@
 # Rust
 [ -f $HOME/.cargo/env ] && source $HOME/.cargo/env
+
+# rust-analyzer
+which rustup >/dev/null 2>&1 &&
+    [ -f $(rustup which --toolchain stable rust-analyzer) ] &&
+    export PATH=$PATH:$(dirname $(rustup which --toolchain stable rust-analyzer))
